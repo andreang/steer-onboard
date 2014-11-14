cdc3ce3851f822bc7b0129b3754b4ebe93da62e9
Andrea <andrea@Andreas-MacBook-Air.local>
2014-11-14 23:20:03 +0800

    session routes; sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   root 'rooms#index'
   resources :rooms
   resources :users, except: [:index, :show]
+  resource :session, only: [:new, :create, :destroy]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
